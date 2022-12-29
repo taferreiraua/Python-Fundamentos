4cb54e6e0b1ef7573abfd28715d79fc722f94906
Thays Ferreira <thaysferreiraua@gmail.com>
2022-12-29 14:46:17 -0300

    Create ex01.py

---
+++ arqv_py/Lista1/ex01.py
@@ -0,0 +1,12 @@
+nome = input('Nome: ')
+idade = int(input('Idade: '))
+altura = float(input('Altura: '))
+peso = float(input('Peso: '))
+nacionalidade = input('Nacionalidade: ')
+
+print(f'\nInformações do usuário:\n'
+      f'Nome: {nome}\n'
+      f'Idade: {idade}\n'
+      f'Altura: {altura}\n'
+      f'Peso: {peso}\n'
+      f'Nacionalidade: {nacionalidade}')
